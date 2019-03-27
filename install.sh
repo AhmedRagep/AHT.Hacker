fdc6b7f95cafb5174c4db5e95e3f534157cfd415
Ahmed-Altorky-Android <46605784+Ahmed-Altorky-Android@users.noreply.github.com>
2019-03-27 08:16:56 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+echo 
+	
+	echo -e -n "pleese enter logen" ;read enter	
+	apt update 
+
+        apt upgrade 
+
+	pkg install figlet 
+
+        apt install get 
+
+        apt install git 
+
+        apt install python3 
+
+        apt install python 
+
+        apt install ruby 
+
+        apt install wget 
+
+        apt install unzip 
+
+        apt install zip 
+
+        apt install nano 
+
+        apt install perl 
+
+        apt install curl 
+
+        apt install proot 
+
+	cd $HOME/AHT.Hacker
+
+	chmod 555 *
+
+figlet -f big "			Done     	"
