1e686c2e67310ddf288c32fff8f036e025beb9c5
Ahmed-Altorky-Android <46605784+Ahmed-Altorky-Android@users.noreply.github.com>
2019-03-30 11:56:55 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,44 @@
+echo 
+	
+	echo -e -n "pleese enter logen~# " ;read enter	
+	apt update 
+
+        apt upgrade 
+
+	pkg install figlet 
+
+        apt install get 
+
+        apt install git 
+
+        apt install python3 
+
+        apt install python 
+
+        apt install ruby 
+
+        apt install wget 
+
+        apt install unzip 
+
+        apt install zip 
+
+        apt install nano 
+
+        apt install perl 
+
+        apt install curl 
+
+        apt install proot
+
+	cd $HOME/AHT.Hacker
+	
+	mkdir home
+	
+	cd $HOME/AHT.Hacker
+	
+	mv apkmeta.sh waite.sh ngrok home
+
+	chmod 555 *
+
+figlet -f big "			Done     	"
