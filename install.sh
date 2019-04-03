199e6b6797c6a3c465b8db6fbb3664842d389cfc
Ahmed-Altorky-Android <46605784+Ahmed-Altorky-Android@users.noreply.github.com>
2019-04-03 19:51:07 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,73 @@
+echo 
+	
+	echo -e -n "pleese enter logen~# " ;read enter
+	cd $HOME/AHT.Hacker
+	bash waite.sh
+
+	apt update 
+
+        apt upgrade 
+
+	pkg install figlet 
+
+        apt install get 
+
+        apt install git 
+
+        apt install python3 
+
+        apt install python 
+
+        apt install ruby 
+
+        apt install wget 
+
+        apt install unzip 
+
+        apt install zip 
+
+        apt install nano 
+
+        apt install perl 
+
+        apt install curl 
+
+        apt install proot
+
+	cd $HOME/AHT.Hacker
+	
+	mkdir home
+	
+	cd $HOME/AHT.Hacker
+	
+	mv apkmeta.sh waite.sh sem.sh sem-t0olss.sh AHT-linux.sh home
+	
+	cd $HOME/AHT.Hacker/home
+	
+	mv apkmeta.sh .apkmeta.sh
+	
+	mv waite.sh .waite.sh
+	
+	mv sem.sh .sem.sh
+	
+	mv sem-t0olss.sh .sem-t0olss.sh
+
+	mv AHT-linux.sh .AHT-linux.sh
+
+	cd $HOME/AHT.Hacker/home
+
+	mkdir .viapk
+
+	cd $HOME/AHT.Hacker
+
+	mv viapk.sh $HOME/AHT.Hacker/home/.viapk
+	
+	cd $HOME/AHT.Hacker
+	
+	chmod 555 *
+
+	cd $HOME/AHT.Hacker
+
+	rm install.sh
+
+figlet -f big "			Done     	"
