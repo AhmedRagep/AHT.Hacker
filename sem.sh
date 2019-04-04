7dc01a18b7c04e3b2a84ad76973f2b5901d30c75
Ahmed-Altorky-Android <46605784+Ahmed-Altorky-Android@users.noreply.github.com>
2019-04-04 18:12:50 +0200

    Add files via upload

---
+++ sem.sh
@@ -0,0 +1,152 @@
+clear
+
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+
+
+
+
+cd $HOME
+
+chat(){
+echo 'def kk(t):' > .sssss.py
+echo '   import sys, time' >> .sssss.py
+echo '   for txt in t + "\n":' >> .sssss.py
+echo '        sys.stdout.write(txt)' >> .sssss.py
+echo '        sys.stdout.flush()' >> .sssss.py
+echo '        time.sleep(9. / 150)' >> .sssss.py
+
+
+echo 'h = "              \033[1;33m[\033[1;32m*\033[1;33m] you welcome in {\033[1;36m AHT.HAKER \033[1;33m} py [\033[1;32m Ahmed.Altorky \033[1;33m] "' >> .sssss.py
+echo 'kk(h)' >> .sssss.py
+python2 .sssss.py
+
+}
+chat2(){
+echo 'def kk(t):' > .ssss.py
+echo '   import sys, time' >> .ssss.py
+echo '   for txt in t + "\n":' >> .ssss.py
+echo '        sys.stdout.write(txt)' >> .ssss.py
+echo '        sys.stdout.flush()' >> .ssss.py
+echo '        time.sleep(9. / 540)' >> .ssss.py
+
+
+
+echo 'a = "   \033[1;34m[ 11 ]\033[1;36m Facebook 		 \033[1;34m[ 22 ]\033[1;36m Youtube"' >> .ssss.py
+echo 'b = "  "' >> .ssss.py
+echo 'c = "  		\033[1;32m[ 33 ]\033[1;33m UPDATE                   \033[1;31m[ 00 ]Exit"' >> .ssss.py
+echo 'd = "  "' >> .ssss.py
+echo 'e = "  \033[1;35m==A=L=T=O=R=K=Y==>>....[ 1 ]\033[1;32m Install Metasploit 	\033[1;35m [ 11 ]\033[1;32m DDOOS Attack "' >> .ssss.py
+echo 'f = "  "' >> .ssss.py
+echo 'g = "  \033[1;35m==A=L=T=O=R=K=Y==>>......[ 2 ]\033[1;32m open metasploit "' >> .ssss.py
+echo 'h = "  "' >> .ssss.py
+echo 'i = "  \033[1;35m==A=L=T=O=R=K=Y==>>........[ 3 ]\033[1;32m Install payload "' >> .ssss.py
+echo 'j = "  "' >> .ssss.py
+echo 'k = "  \033[1;35m==A=L=T=O=R=K=Y==>>..........[ 4 ]\033[1;32m Hacking photo "' >> .ssss.py
+echo 'l = "  "' >> .ssss.py
+echo 'm = "  \033[1;35m==A=L=T=O=R=K=Y==>>............[ 5 ]\033[1;32m Ngrok "' >> .ssss.py
+echo 'n = "  "' >> .ssss.py
+echo 'o = "  \033[1;35m==A=L=T=O=R=K=Y==>>..........[ 6 ]\033[1;32m AHT T0olss  "' >> .ssss.py
+echo 'p = "  "' >> .ssss.py
+echo 'q = "  \033[1;35m==A=L=T=O=R=K=Y==>>........[ 7 ]\033[1;32m Send SmS "' >> .ssss.py
+echo 'r = "  "' >> .ssss.py
+echo 's = "  \033[1;35m==A=L=T=O=R=K=Y==>>......[ 8 ]\033[1;32m Emails  "' >> .ssss.py
+echo 't = "  "' >> .ssss.py
+echo 'u = "  \033[1;35m==A=L=T=O=R=K=Y==>>....[ 9 ]\033[1;31m VIRUS AHT  "' >> .ssss.py
+echo 'v = "  "' >> .ssss.py
+echo 'w = "  \033[1;35m==A=L=T=O=R=K=Y==>>.[ 10 ]\033[1;32m Distributions Linux  "' >> .ssss.py
+echo 'x = "  "' >> .ssss.py
+
+
+echo 'kk(a)' >> .ssss.py
+echo 'kk(b)' >> .ssss.py
+echo 'kk(c)' >> .ssss.py
+echo 'kk(d)' >> .ssss.py
+echo 'kk(e)' >> .ssss.py
+echo 'kk(f)' >> .ssss.py
+echo 'kk(g)' >> .ssss.py
+echo 'kk(h)' >> .ssss.py
+echo 'kk(i)' >> .ssss.py
+echo 'kk(j)' >> .ssss.py
+echo 'kk(k)' >> .ssss.py
+echo 'kk(l)' >> .ssss.py
+echo 'kk(m)' >> .ssss.py
+echo 'kk(n)' >> .ssss.py
+echo 'kk(o)' >> .ssss.py
+echo 'kk(p)' >> .ssss.py
+echo 'kk(q)' >> .ssss.py
+echo 'kk(r)' >> .ssss.py
+echo 'kk(s)' >> .ssss.py
+echo 'kk(t)' >> .ssss.py
+echo 'kk(u)' >> .ssss.py
+echo 'kk(v)' >> .ssss.py
+echo 'kk(w)' >> .ssss.py
+echo 'kk(x)' >> .ssss.py
+python2 .ssss.py
+
+}
+
+
+
+echo -e "$green "
+echo -e """
+
+	           ▄▄          ▄▄     ▄▄    ▄ ▄ ▄▄ ▄ ▄ ▄
+	          ████         ██     ██    ▄.▄.▄▄.▄▄.▄▄
+	          ████         ██     ██         █
+	         ██  ██        ██████ ██          █
+	         ██████        ██     ██         █   ▄   ▄
+	        ▄██  ██▄       ██     ██          █   ▄ ▄
+	        ▀▀    ▀▀       ▀▀     ▄▄         █|    ▄  3.0
+"""
+
+chat
+echo -e "$red"
+echo -e               			                     	$cyan '   Welcome to----->{'$green'2019'$cyan'}'$blue
+sleep 0.1
+
+cat << "EOF"
+                              ..
+                             *__*
+                            * .. *
+                           / >.^.<\
+                          /.. . . .\
+                         /.    ^   .\
+                 _____,-"~    \ /   "~"-._____
+             ,-~"     "~-.           .-~"     "~-.
+           ,^             ^. `. .' ,^             ^.
+          /  A     H    T   \  ^  /    H A K E R    \
+         Y___________________Y   Y___________________Y
+         | |^~"|^   _ ^|"~^| |   | |"~"|^ _   ^|"~"| |
+         | !   l   (_) !   ! l   | !   l (_)   !   ! |
+         l  \  `\.___,/'  /  !   l  \  `\.___,/'  /  !
+          \  ^.         ,^  /!   !\  ^.         ,^  /
+           ^.  ~-------~  ,^\`v-v'/^.  ~-------~  ,^
+             ~-._______,-~   }---{   ~-._______,-~
+               \      ##  #   #     ### ###    /
+                \ \  #  # #   ### # #__ #  #  /
+                 \   #$$# #$$$# # # #   #  # /
+                  ]_ #  # #   # # # ### # # _[
+                     '# - $ -  $ - $ -  #'
+                       '###############'
+EOF
+echo -e " ===================================================================="
+sleep 0.1
+echo -e "                        $bluo H $green A $red k $purple E $cyan R "
+sleep 0.2
+echo -e $cyan "================================================================="
+echo -e $cyan "================================================================="
+echo ""
+echo ""
+
+chat2
